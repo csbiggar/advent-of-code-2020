98b51ea011d415e0dc14c8d24dfec9926fb8d0da
Carolyn Biggar <carolyn.biggar@johnlewis.co.uk>
2020-12-01 22:39:19 +0000

    [D1#2 Result] Find 3 components that add up to 2020
    in need of refactoring

--- src/main/kotlin/day1/ExpensesReport.kt
+++ src/main/kotlin/day1/ExpensesReport.kt
@@ -5,23 +5,62 @@ import java.lang.IllegalArgumentException
 
 private val input = FileReader("/day1/input1.txt").readInts()
 
-fun main() {
-    val result = findComponentsSummingTo2020(input)
 
-    println(result)
-    println("Answer: ${result.first * result.second}")
+/*
+For 2 components: Numbers [28, 1992], Result: 55776
+For 3 components: Numbers [983, 314, 723], Result: 223162626
+ */
+
+fun main() {
+    val resultFor2 = findComponentsSummingTo2020(input, 2)
+    printResult(resultFor2)
+
+    val resultFor3 = findComponentsSummingTo2020(input, 3)
+    printResult(resultFor3)
 }
 
-tailrec fun findComponentsSummingTo2020(report: List<Int>): Pair<Int, Int> {
+private fun printResult(result: List<Int>) {
+    println("For ${result.size} components: Numbers $result, Result: ${result.reduce { first, second -> first * second }}")
+}
+
+fun findComponentsSummingTo2020(report: List<Int>, numberOfComponents: Int): List<Int> {
+
+    if (numberOfComponents == 2) return findTwoComponentsSummingTo2020(0, report)
+        ?: throw IllegalArgumentException("No numbers sum to 2020")
+
+    if (numberOfComponents == 3) return findThree(report) ?: throw IllegalArgumentException("No numbers sum to 2020")
+    else TODO()
+
+}
+
+tailrec fun findTwoComponentsSummingTo2020(base: Int = 0, report: List<Int>): List<Int>? {
 
     val firstNumber = report.first()
     val possibleSecondNumbers = report.drop(1)
 
-    possibleSecondNumbers.forEach { secondNumber ->
-        if (secondNumber + firstNumber == 2020) return Pair(firstNumber, secondNumber)
+    val secondNumber = possibleSecondNumbers
+        .firstOrNull { secondNumber -> base + firstNumber + secondNumber == 2020 }
+
+    if (secondNumber != null) {
+        // found!
+        return listOf(firstNumber, secondNumber)
     }
 
-    if (possibleSecondNumbers.size < 2) throw IllegalArgumentException("No numbers sum to 2020")
+    if (possibleSecondNumbers.size < 2) return null
 
-    return findComponentsSummingTo2020(possibleSecondNumbers)
+    return findTwoComponentsSummingTo2020(base, possibleSecondNumbers)
+}
+
+tailrec fun findThree(report: List<Int>): List<Int>? {
+    val first = report.first()
+    val possibleSecondAndThirdNumbers = report.drop(1)
+    val secondAndThird = findTwoComponentsSummingTo2020(first, possibleSecondAndThirdNumbers)
+
+    if (secondAndThird != null) {
+        return listOf(first) + secondAndThird
+    }
+
+    if (possibleSecondAndThirdNumbers.size < 3) throw IllegalArgumentException("No numbers sum to 2020")
+
+    return findThree(possibleSecondAndThirdNumbers)
 }
--- src/test/kotlin/day1/ExpensesReportKtTest.kt
+++ src/test/kotlin/day1/ExpensesReportKtTest.kt
@@ -1,7 +1,6 @@
 package day1
 
 import org.assertj.core.api.Assertions.assertThat
-import org.junit.jupiter.api.Disabled
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.assertThrows
 import java.lang.IllegalArgumentException
@@ -11,26 +10,26 @@ internal class ExpensesReportKtTest {
     @Test
     fun `should return the only two numbers if they sum to 2020`() {
         val input = listOf(2019, 1)
-        assertThat(findComponentsSummingTo2020(input).toList()).containsExactlyInAnyOrder(1, 2019)
+        assertThat(findComponentsSummingTo2020(input, 2)).containsExactlyInAnyOrder(1, 2019)
     }
 
     @Test
     fun `should return the only two numbers if they sum to 2020 the other way around`() {
         val input = listOf(2019, 1)
-        assertThat(findComponentsSummingTo2020(input).toList()).containsExactlyInAnyOrder(1, 2019)
+        assertThat(findComponentsSummingTo2020(input, 2)).containsExactlyInAnyOrder(1, 2019)
     }
 
     @Test
     fun `should return the two numbers that sum to 2020 when they are not first in the list`() {
         val input = listOf(1, 2, 2019)
-        assertThat(findComponentsSummingTo2020(input).toList()).containsExactlyInAnyOrder(1, 2019)
+        assertThat(findComponentsSummingTo2020(input, 2)).containsExactlyInAnyOrder(1, 2019)
 
     }
 
     @Test
     fun `should throw exception if no numbers sum to 2020`() {
         val input = listOf(2018, 1)
-        assertThrows<IllegalArgumentException> { findComponentsSummingTo2020(input) }
+        assertThrows<IllegalArgumentException> { findComponentsSummingTo2020(input, 2) }
     }
 
     @Test
@@ -44,6 +43,35 @@ internal class ExpensesReportKtTest {
             1456
         )
 
-        assertThat(findComponentsSummingTo2020(input).toList()).containsExactlyInAnyOrder(1721, 299)
+        assertThat(findComponentsSummingTo2020(input, 2)).containsExactlyInAnyOrder(1721, 299)
+    }
+
+    @Test
+    fun `should return the three numbers even when it hits the two that add up to 2020 first`() {
+        val input = listOf(33, 55, 2019, 1, 2015, 3, 2)
+
+        // When
+        val result = findComponentsSummingTo2020(input, 3)
+
+        // Then
+        assertThat(result).containsExactlyInAnyOrder(2015, 3, 2)
+    }
+
+    @Test
+    fun `should find the three numbers whose sum is 2020`() {
+        val input = listOf(
+            1721,
+            979,
+            366,
+            299,
+            675,
+            1456
+        )
+
+        // When
+        val result = findComponentsSummingTo2020(input, 3)
+
+        // Then
+        assertThat(result).containsExactlyInAnyOrder(979, 366, 675)
     }
 }
