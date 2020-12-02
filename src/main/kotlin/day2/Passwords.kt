f60e27659a865e2aaae6e2292221c94b58e2d78b
Carolyn Biggar <carolyn.biggar@johnlewis.co.uk>
2020-12-02 22:35:58 +0000

    [D2#2 Result] Find number of valid passwords using second rules criteria

--- src/main/kotlin/day2/Passwords.kt
+++ src/main/kotlin/day2/Passwords.kt
@@ -8,31 +8,48 @@ data class PasswordEntry(val password: String, val requiredLetter: Char, val min
 
 D2#1 418 valid passwords
 
+D2#2 616 valid passwords
+
  */
 private val input = FileReader("/day2/input.txt").readLines()
 
 fun main() {
-    val result = findValidPasswords(input)
-    println(result.size)
+    val result1 = findValidPasswordsUsingFirstRules(input)
+    println("Using first rules: ${result1.size}")
+
+    val result2 = findValidPasswordsUsingSecondRules(input)
+    println("Using second rules: ${result2.size}")
 }
 
-
-fun findValidPasswords(input: List<String>): List<PasswordEntry> {
-    val passwordEntries = input.map {
-        val bits = it.split(" ")
-        PasswordEntry(
-            minOccurrence = bits[0].substringBefore("-").toInt(),
-            maxOccurrence = bits[0].substringAfter("-").toInt(),
-            requiredLetter = bits[1].toCharArray().first(),
-            password = bits[2]
-        )
-    }
-
-    return passwordEntries
+fun findValidPasswordsUsingFirstRules(input: List<String>): List<PasswordEntry> {
+    return mapToPasswordEntries(input)
         .filter {
             it.password.countLetter(it.requiredLetter) >= it.minOccurrence
                     && it.password.countLetter(it.requiredLetter) <= it.maxOccurrence
         }
+}
+
+fun findValidPasswordsUsingSecondRules(input: List<String>): List<PasswordEntry> {
+    return mapToPasswordEntries(input)
+        .filter { entry ->
+            positionValues(entry).filter { it == entry.requiredLetter }.size == 1
+        }
+
+}
+
+private fun positionValues(entry: PasswordEntry) = listOf(
+    entry.password[entry.minOccurrence - 1],
+    entry.password[entry.maxOccurrence - 1]
+)
+
+private fun mapToPasswordEntries(input: List<String>) = input.map {
+    val bits = it.split(" ")
+    PasswordEntry(
+        minOccurrence = bits[0].substringBefore("-").toInt(),
+        maxOccurrence = bits[0].substringAfter("-").toInt(),
+        requiredLetter = bits[1].toCharArray().first(),
+        password = bits[2]
+    )
 }
 
 
--- src/test/kotlin/day2/PasswordsKtTest.kt
+++ src/test/kotlin/day2/PasswordsKtTest.kt
@@ -1,59 +1,96 @@
 package day2
 
 import org.assertj.core.api.Assertions.assertThat
-import org.junit.jupiter.api.Assertions.*
-import org.junit.jupiter.api.Disabled
+import org.junit.jupiter.api.Nested
 import org.junit.jupiter.api.Test
 
 internal class PasswordsKtTest {
 
-    @Test
-    fun `should include  valid password`() {
-        val input = listOf("1-1 a: a")
+    @Nested
+    inner class FirstRules {
+        @Test
+        fun `should include  valid password`() {
+            val input = listOf("1-1 a: a")
 
-        val result = findValidPasswords(input)
+            val result = findValidPasswordsUsingFirstRules(input)
 
-        val expected = PasswordEntry(password = "a", requiredLetter = 'a', minOccurrence = 1, maxOccurrence = 1)
-        assertThat(result).containsExactly(expected)
+            val expected = PasswordEntry(password = "a", requiredLetter = 'a', minOccurrence = 1, maxOccurrence = 1)
+            assertThat(result).containsExactly(expected)
+        }
+
+        @Test
+        fun `should exclude password with no occurrences of the letter`() {
+            val input = listOf("1-1 a: b")
+
+            val result = findValidPasswordsUsingFirstRules(input)
+
+            assertThat(result).isEmpty()
+        }
+
+        @Test
+        fun `should exclude password with too few occurrences  of letter`() {
+            val input = listOf("2-2 a: a")
+
+            val result = findValidPasswordsUsingFirstRules(input)
+
+            assertThat(result).isEmpty()
+        }
+
+        @Test
+        fun `should exclude password with too many of letter`() {
+            val input = listOf("2-2 a: aaa")
+
+            val result = findValidPasswordsUsingFirstRules(input)
+
+            assertThat(result).isEmpty()
+        }
+
+        @Test
+        fun `should find two valid passwords in example list`() {
+            val input = listOf(
+                "1-3 a: abcde",
+                "1-3 b: cdefg",
+                "2-9 c: ccccccccc"
+            )
+
+            val result = findValidPasswordsUsingFirstRules(input)
+
+            assertThat(result.map { it.password }).containsExactlyInAnyOrder("abcde", "ccccccccc")
+        }
     }
 
-    @Test
-    fun `should exclude password with no occurrences of the letter`() {
-        val input = listOf("1-1 a: b")
+    @Nested
+    inner class SecondRules {
+        @Test
+        fun `should pass if first position contains given letter`() {
+            val input = listOf("1-2 a: abc")
 
-        val result = findValidPasswords(input)
+            val result = findValidPasswordsUsingSecondRules(input)
 
-        assertThat(result).isEmpty()
-    }
+            val expected = PasswordEntry(password = "abc", requiredLetter = 'a', minOccurrence = 1, maxOccurrence = 2)
+            assertThat(result).containsExactly(expected)
+        }
 
-    @Test
-    fun `should exclude password with too few occurrences  of letter`() {
-        val input = listOf("2-2 a: a")
+        @Test
+        fun `should fail if both first and second position contains given letter`() {
+            val input = listOf("1-2 a: aa")
 
-        val result = findValidPasswords(input)
+            val result = findValidPasswordsUsingSecondRules(input)
 
-        assertThat(result).isEmpty()
-    }
+            assertThat(result).isEmpty()
+        }
 
-    @Test
-    fun `should exclude password with too many of letter`() {
-        val input = listOf("2-2 a: aaa")
+        @Test
+        fun `should find two valid passwords in example list`() {
+            val input = listOf(
+                "1-3 a: abcde",
+                "1-3 b: cdefg",
+                "2-9 c: ccccccccc"
+            )
 
-        val result = findValidPasswords(input)
+            val result = findValidPasswordsUsingSecondRules(input)
 
-        assertThat(result).isEmpty()
-    }
-
-    @Test
-    fun `should find two valid passwords in example list`() {
-        val input = listOf(
-            "1-3 a: abcde",
-            "1-3 b: cdefg",
-            "2-9 c: ccccccccc"
-        )
-
-        val result = findValidPasswords(input)
-
-        assertThat(result.map { it.password }).containsExactlyInAnyOrder("abcde", "ccccccccc")
+            assertThat(result.map { it.password }).containsExactlyInAnyOrder("abcde")
+        }
     }
 }
