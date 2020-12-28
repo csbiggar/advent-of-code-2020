571e15322966a9a21a14c87a4d20369ebec11be1
Carolyn Biggar <carolyn.biggar@johnlewis.co.uk>
2020-12-28 19:27:15 +0000

    [D8#2] [NO BUILD] Add tests for part 2, not implemented yet

--- src/main/kotlin/day10/AdapterArray.kt
+++ src/main/kotlin/day10/AdapterArray.kt
@@ -3,13 +3,13 @@ package day10
 import helpers.FileReader
 import kotlin.math.absoluteValue
 
-private val input = FileReader("/day10/input.txt").readLines().map { it.toInt() }
+private val puzzleInput = FileReader("/day10/input.txt").readLines().map { it.toInt() }
 
 
 data class Counts(val diffOne: Int, val diffThree: Int)
 
 fun main() {
-    val result = findDiffCounts(input)
+    val result = findDiffCounts(puzzleInput)
     println("$result, product is ${result.diffOne * result.diffThree}")
 }
 
@@ -28,4 +28,34 @@ fun findDiffCounts(input: List<Int>): Counts {
         diffOne = result[1]?.size ?: 0,
         diffThree = result[3]?.size ?: 0
     )
+}
+
+
+class Combinations(unsortedInput: List<Int>) {
+
+    private val sortedOriginalInput = unsortedInput.sorted()
+
+    init {
+        println(sortedOriginalInput)
+    }
+
+    fun findCombinations(): Long {
+        TODO()
+    }
+
+    /*
+[1,2] [2]
+
+[1,2,3] [1,3] [2,3] [3]
+
+(0), 1, 4, 5, 6, 7, 10, 11, 12, 15, 16, 19, (22)
+(0), 1, 4, 5, 6, 7, 10, 12, 15, 16, 19, (22)
+(0), 1, 4, 5, 7, 10, 11, 12, 15, 16, 19, (22)
+(0), 1, 4, 5, 7, 10, 12, 15, 16, 19, (22)
+(0), 1, 4, 6, 7, 10, 11, 12, 15, 16, 19, (22)
+(0), 1, 4, 6, 7, 10, 12, 15, 16, 19, (22)
+(0), 1, 4, 7, 10, 11, 12, 15, 16, 19, (22)
+(0), 1, 4, 7, 10, 12, 15, 16, 19, (22)
+ */
+
 }
--- src/test/kotlin/day10/AdapterArrayTest.kt
+++ src/test/kotlin/day10/AdapterArrayTest.kt
@@ -2,12 +2,49 @@ package day10
 
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Test
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.Arguments
+import org.junit.jupiter.params.provider.MethodSource
+import java.util.stream.Stream
 
 internal class AdapterArrayTest {
 
+    companion object {
+
+        @JvmStatic
+        fun combinationsTests(): Stream<Arguments> {
+            return Stream.of(
+                combinationsArguments(input = listOf(1), expectedResult = 1),
+                /*
+                [1,2] [2]
+                 */
+                combinationsArguments(input = listOf(1, 2), expectedResult = 2),
+                /*
+                [1,2,3] [1,3] [2,3] [3]
+                 */
+                combinationsArguments(input = listOf(1, 2, 3), expectedResult = 4),
+                combinationsArguments(input = example1Input, expectedResult = 8),
+                combinationsArguments(input = example2Input, expectedResult = 19208),
+            )
+        }
+
+        private fun combinationsArguments(input: List<Int>, expectedResult: Long) = Arguments.of(
+            input,
+            expectedResult
+        )
+    }
+
+    @ParameterizedTest
+    @MethodSource("combinationsTests")
+    fun `find number of possible combinations`(
+        input: List<Int>,
+        expectedResult: Long
+    ) {
+        assertThat(Combinations(input).findCombinations()).isEqualTo(expectedResult)
+    }
 
     @Test
-    fun `example set`() {
+    fun `should count number of 3 and number of 1 step differences`() {
 
         val input = listOf(
             16,
@@ -26,3 +63,39 @@ internal class AdapterArrayTest {
         assertThat(findDiffCounts(input)).isEqualTo(Counts(diffOne = 7, diffThree = 5))
     }
 }
+
+private val example1Input = listOf(16, 10, 15, 5, 1, 11, 7, 19, 6, 12, 4)
+
+private val example2Input = listOf(
+    28,
+    33,
+    18,
+    42,
+    31,
+    14,
+    46,
+    20,
+    48,
+    47,
+    24,
+    23,
+    49,
+    45,
+    19,
+    38,
+    39,
+    11,
+    1,
+    32,
+    25,
+    35,
+    8,
+    17,
+    7,
+    9,
+    4,
+    2,
+    34,
+    10,
+    3
+)
